bc10c2d4ed8fa9715049bf64c99d7982ab3cc70e
justEstif <95101485+justEstif@users.noreply.github.com>
2022-05-17 14:13:23 -0400

    completed React interactivity: events and state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Todo(props) {
-  const { name, completed, id } = props
+  const { name, completed, id, deleteTask } = props;
   return (
     <li key={id} className="todo stack-small">
       <div className="c-cb">
-        <input id={id} type="checkbox" defaultChecked={completed} />
+        <input
+          id={id}
+          type="checkbox"
+          defaultChecked={completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
         <label className="todo-label" htmlFor={id}>
           {name}
         </label>
@@ -14,7 +19,11 @@ function Todo(props) {
         <button type="button" className="btn">
           Edit <span className="visually-hidden">Eat</span>
         </button>
-        <button type="button" className="btn btn__danger">
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => deleteTask(id)}
+        >
           Delete <span className="visually-hidden">Eat</span>
         </button>
       </div>
