f33381b98d610dfe6dc2487e159a1f16d09d4584
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-17 15:14:07 -0400

    vite react-ts with tw-macro

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+  return <div>Hello From App</div>;
+};
+
+export default App;
---
+++ todo-app/src/styles/Input.tsx
@@ -0,0 +1,7 @@
+import tw, { styled } from "twin.macro";
+
+const Input = tw.input`
+  border-red-500
+`;
+
+export { Input };
